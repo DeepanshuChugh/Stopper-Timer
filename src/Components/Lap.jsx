507c11bbb7dba35bbba8870e39a1a1fd92713d78
DeepanshuChugh <deepanshuchugh24@gmail.com>
2022-05-27 12:07:07 +0530

    Lap Functionality

---
+++ src/Components/Lap.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Lap = ({lap,ind}) => {
+  return (
+    <div key={ind}>{lap}</div>
+  )
+}
+
+export default Lap
--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import Lap from './Lap'
 import styles from './stop.module.css'
 
 const Stopwatch = () => {
-    
+ 
     const [timer,setTimer]=useState(0)
     const [timerOn,setTimerOn]=useState(false)
-    
+    const [lap,setLap]=useState([])
+    let m =('0'+Math.floor(timer/60000)%60).slice(-2);
+    let s =('0'+Math.floor(timer/1000)%60).slice(-2);
+    let ms =('0'+(timer/10)%100).slice(-2);
     useEffect(()=>{
         let id = null;
         if(timerOn)
@@ -15,18 +19,18 @@ const Stopwatch = () => {
             }, 10);
         }
         else{
-            clearInterval(id)
+            clearInterval(id)  
         }
-
-        return ()=>{
+        
+        return ()=>{        
             clearInterval(id)
         }
 
     },[timerOn])
-    
-    
-    
-    
+
+//    const lap =()=> {
+//     console.log(m,':',s,':',ms);
+//    }
  
   return (
     <>
@@ -34,9 +38,9 @@ const Stopwatch = () => {
         <h1>Stopwatch</h1>
     </div>
     <div>
-        <span><h1 className={styles.block}>{('0'+Math.floor(timer/60000)%60).slice(-2)}</h1>m </span>
-        <span><h1 className={styles.block}>{('0'+Math.floor(timer/1000)%60).slice(-2)}</h1>s </span>
-        <span><h1 className={styles.block}>{('0'+(timer/10)%100).slice(-2)}</h1>ms</span>
+        <span><h1 className={styles.block}>{m}</h1>m </span>
+        <span><h1 className={styles.block}>{s}</h1>s </span>
+        <span><h1 className={styles.block}>{ms}</h1>ms</span>
     </div>
     {timer===0 && !timerOn &&(
         <button onClick={()=>{setTimerOn(true)}}>Start</button>
@@ -50,6 +54,8 @@ const Stopwatch = () => {
     {timer>0 && !timerOn &&(
     <button onClick={()=>{setTimer(0)}}>Reset</button>
     )}
+    <button onClick={()=>{setLap([[`${m}:${s}:${ms}`],...lap])}}>LAP</button>
+    {lap.map((el,ind)=>(<Lap lap={el} ind={ind}/>))}
     </>
   )
 }
